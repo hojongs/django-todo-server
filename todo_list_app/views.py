778f3d00d9125d867e1b1fc1441d6106f4aee217
hojong_home <hojong.jjh@gmail.com>
2019-10-25 17:24:44 +0900

    add 2 template views (detail, modify)

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("todo_list_app index")
+
+
+def detail(request, todo_id):
+    return HttpResponse("detail page of %d" % todo_id)
+
+
+def modify(request, todo_id):
+    return HttpResponse("modify page of %d" % todo_id)
