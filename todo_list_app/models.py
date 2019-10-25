46ba1346106717dde6bbbe3ad69d5f2ac55d708f
hojong_home <hojong.jjh@gmail.com>
2019-10-25 17:15:39 +0900

    Fix Todo model (added blank attribute)

--- todo_list_app/models.py
+++ todo_list_app/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class Todo(models.Model):
     todo_name = models.CharField(max_length=256)
     pub_date = models.DateTimeField('date published', default=timezone.now)
-    parent_todo = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
-    priority = models.IntegerField(null=True)
+    parent_todo = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    priority = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.todo_name
